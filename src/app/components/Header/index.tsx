c4fc3c63f63dd40e1f8169379241f8d16f2a3e47
maxinelobato <maxinelobato89@hotmail.com>
2023-11-13 13:43:02 -0300

    changes

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <>
       <section className="relative max-h-[87vh] bg-no-repeat bg-center bg-cover bg-[url(https://img.freepik.com/fotos-gratis/criminoso-algemado_53876-139638.jpg?w=1060&t=st=1699825563~exp=1699826163~hmac=7c8fced5e11338e3387fbf16321ad6dbb9d564fc6988b9586f471a353c3d425f)]">
         <div className="absolute inset-0 bg-stone-950/80"></div>
-        <div className="relative mx-auto max-w-screen-xl px-4 py-20 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+        <div className="max-w-screen-xl px-4 py-20 relative mx-auto lg:h-screen lg:items-center lg:px-8 sm:px-6 lg:flex">
           <div className="max-w-screen-2xl text-center sm:text-left">
             <h1
               className="text-3xl sm:text-5xl text-white font-extrabold"
