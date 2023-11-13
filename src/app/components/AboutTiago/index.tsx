142d71b7727f67a4fc09cfff2a5df6d6910a5697
maxinelobato <maxinelobato89@hotmail.com>
2023-11-12 21:11:47 -0300

    changes

--- src/app/components/AboutTiago/index.tsx
+++ src/app/components/AboutTiago/index.tsx
@@ -5,48 +5,6 @@ import tiago from "/public/drtiago.jpg";
 export default function AboutTiago() {
   return (
     <>
-      {/* <section className="overflow-hidden bg-yellow-950 sm:grid sm:grid-cols-2 sm:items-center">
-        <div className="p-8 md:p-12 lg:px-16 lg:py-0">
-          <div className="mx-auto max-w-md text-center sm:text-left">
-            <div className="max-w-xl mb-6">
-              <div>
-                <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-stone-950 uppercase rounded-full bg-white">
-                  Sobre
-                </p>
-              </div>
-              <h2 className="max-w-lg mb-6 text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-                Tiago Lenoir
-                <br className="hidden md:block" />
-                <span className="inline-block text-white font-thin text-2xl">
-                  OAB/MG 102.238
-                </span>
-              </h2>
-              <p className="text-sm font-semibold text-white md:text-lg">
-                Tiago Lenoir é advogado criminalista com mais de 18 anos de
-                experiência na área penal. Professor Universitário. Mestre em
-                Direito e especialista em Criminologia pela Universidade de
-                Itaúna.
-              </p>
-              <p className="mt-4 font-semibold text-sm text-white md:text-lg">
-                Especialista em Criminologia pelo IEC-PUC MINAS. Graduado em
-                Direito pela PUC Minas. Auditor do Tribunal de Justiça
-                Desportiva da FMF.
-              </p>
-            </div>
-            <div className="mt-4 md:mt-8">
-              <ButtonHero />
-            </div>
-          </div>
-        </div>
-
-        <Image
-          alt="Tiago About"
-          src={tiago}
-          width={600}
-          height={600}
-          className="h-full w-full object-cover "
-        />
-      </section> */}
       <div className="bg-stone-950 relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
         <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-2/4 lg:max-w-full lg:absolute xl:pb-0">
           <svg
--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -5,42 +5,6 @@ import lenoir from "/public/lenoir.svg";
 export default function Header() {
   return (
     <>
-      {/* <section className="hero min-h-[85vh] bg-no-repeat bg-center bg-cover bg-[url(https://img.freepik.com/fotos-gratis/vista-superior-bonito-conceito-de-familia-arranjo_23-2148485826.jpg?w=740&t=st=1699645401~exp=1699646001~hmac=7023570bcc50b3c192ba16d8258dbd757b4d4f532602c7d218cc359dae79a0bf)] sm:bg-[url(https://img.freepik.com/fotos-gratis/silhueta-de-familia-feliz-na-sunset_1303-22466.jpg?w=1060&t=st=1699645478~exp=1699646078~hmac=32459f080dfcba73cd84aa1855f089aca0a4da0a14e02c7bd620926fa1de241f)] sm:bg-cover md:bg-[url(https://img.freepik.com/fotos-gratis/silhueta-de-familia-feliz-na-sunset_1303-22466.jpg?w=1060&t=st=1699645478~exp=1699646078~hmac=32459f080dfcba73cd84aa1855f089aca0a4da0a14e02c7bd620926fa1de241f)] md:bg-cover lg:bg-[url(https://img.freepik.com/fotos-gratis/silhueta-de-familia-feliz-na-sunset_1303-22466.jpg?w=1060&t=st=1699645478~exp=1699646078~hmac=32459f080dfcba73cd84aa1855f089aca0a4da0a14e02c7bd620926fa1de241f)] lg:bg-cover">
-        <div className="hero-overlay bg-gradient-to-r from-teal-950 to-transparent"></div>
-          <div className="max-w-screen-2xl text-center">
-            <h1
-              className="mb-6 text-3xl sm:text-5xl font-bold text-white uppercase"
-              style={{ textShadow: "0px 0px 8px #000000" }}
-            >
-              precisa de uma advogada de família?
-            </h1>
-            <p
-              className="text-sm sm:text-2xl text-white"
-              style={{ textShadow: "0px 0px 8px #000000" }}
-            >
-              Protegendo seu futuro familiar com{" "}
-              <span className="text-stone-400">
-                experiência jurídica especializada!
-              </span>
-            </p>
-            <p
-              className="mb-8 text-sm sm:text-2xl text-white"
-              style={{ textShadow: "0px 0px 10px #000000" }}
-            >
-              Atendimento em{" "}
-              <span className="text-stone-400">Belo Horizonte</span>
-            </p>
-            <ButtonHero />
-            <div className="mt-16">
-              <Image
-                className="self-end w-48 mx-auto md:w-auto lg:w-72"
-                src={lenoir}
-                sizes="100vw"
-                alt="Logo Lenoir"
-              />
-            </div>
-          </div>
-      </section> */}
       <section className="relative max-h-[87vh] bg-no-repeat bg-center bg-cover bg-[url(https://img.freepik.com/fotos-gratis/criminoso-algemado_53876-139638.jpg?w=1060&t=st=1699825563~exp=1699826163~hmac=7c8fced5e11338e3387fbf16321ad6dbb9d564fc6988b9586f471a353c3d425f)]">
         <div className="absolute inset-0 bg-stone-950/80"></div>
         <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
