d0a4c57afa91ce13cad774c6eef9443625058cc4
maxinelobato <maxinelobato89@hotmail.com>
2023-11-22 23:25:15 -0300

    changes

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -5,7 +5,7 @@ import lenoir from "/public/lenoir.svg";
 export default function Header() {
   return (
     <>
-      <section className="relative max-h-[90vh] bg-no-repeat bg-center bg-cover lg:max-h-[85vh] lg:bg-cover">
+      <section className="relative max-h-[90vh] bg-no-repeat bg-center bg-cover bg-bg1 lg:max-h-[85vh] lg:bg-cover">
         <div className="absolute inset-0 bg-gradient-to-r from-stone-950 to-transparent"></div>
         <div className="relative max-w-screen-xl mx-auto px-4 py-20 lg:py-52 lg:flex lg:items-center lg:h-screen lg:px-8 sm:px-6">
           <div className="max-w-screen-2xl text-center sm:text-left">
