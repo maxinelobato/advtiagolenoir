01cd0622bca49999bc6710ee06b2988a00338ae0
maxinelobato <maxinelobato89@hotmail.com>
2023-11-26 18:50:29 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
-import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,33 +22,33 @@ export default function RootLayout({
         name="facebook-domain-verification"
         content="0pp7qrk8a62xbewr980vkh2xrevbua"
       /> */}
-      <Suspense>
-        <Script
-          id="gtag-base"
-          strategy="lazyOnload"
-          dangerouslySetInnerHTML={{
-            __html: `
+
+      <Script
+        id="gtag-base"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer', 'GTM-PLMR26DV');
           `,
-          }}
-        ></Script>
-      </Suspense>
+        }}
+      ></Script>
+
       <body className={inter.className}>
         {children}
-        <Suspense>
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=GTM-PLMR26DV`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
-        </Suspense>
+
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-PLMR26DV`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
       </body>
     </html>
   );
